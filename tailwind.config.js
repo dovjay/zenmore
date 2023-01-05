4887b2436252ac4c1f1b9f0abd0a3fd1a10779bf
Dovjay <dovansanjaya24@gmail.com>
2023-01-05 16:17:11 +0700

    delete some next file and create bangboo bg slide animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bangbooBgSlide: {
+          '0%': { 'background-position': '0 0' },
+          '100%': { 'background-position': '-25% 51%'}
+        }
+      },
+      animation: {
+        'bangboo-bg-slide': 'bangbooBgSlide 10s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
